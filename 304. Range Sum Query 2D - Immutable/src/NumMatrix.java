2dd5117e79d73e9956e708e6cce2867ce5165178
derek <harrison.derek.w@gmail.com>
2022-09-23 13:28:18 +0200

    refactor range sum query

--- 304. Range Sum Query 2D - Immutable/src/NumMatrix.java
+++ 304. Range Sum Query 2D - Immutable/src/NumMatrix.java
@@ -1,9 +1,9 @@
 class NumMatrix {
 
-    int[][] dp;
+    int[][] areas;
     int[][] matrix;
 
-    int makeDP(int m, int n, int[][] dp) {
+    int computeAreas(int m, int n, int[][] areas) {
 
         int res = 0;
 
@@ -12,29 +12,27 @@ class NumMatrix {
             return 0;
         }
 
-        // Do not recompute sums
-        if(dp[m][n] != 0) {
-            return dp[m][n];
+        // Do not recompute areas
+        if(areas[m][n] != 0) {
+            return areas[m][n];
         }
 
         // Compute sum of a (1 x 1) matrix
         if(m == 1 && n == 1) {
-            return dp[m][n] = matrix[m - 1][n - 1];
+            return areas[m][n] = matrix[m - 1][n - 1];
         }
 
         // Compute sum in the range (m, n - 1)
-        int sum1 = makeDP(m, n - 1, dp);
+        int sum1 = computeAreas(m, n - 1, areas);
         // Compute sum in the range (m - 1, n)
-        int sum2 = makeDP(m - 1, n, dp);
+        int sum2 = computeAreas(m - 1, n, areas);
         // Compute sum in the range (m - 1, n - 1)
-        int sum3 = makeDP(m - 1, n - 1, dp);
+        int sum3 = computeAreas(m - 1, n - 1, areas);
 
-        res = matrix[m - 1][n - 1] + sum1 + sum2 - sum3;
+        // Compute area sum
+        areas[m][n] = matrix[m - 1][n - 1] + sum1 + sum2 - sum3;
 
-        // Store results in table
-        dp[m][n] = res;
-
-        return res;
+        return areas[m][n];
     }
 
     public NumMatrix(int[][] matrix) {
@@ -44,16 +42,16 @@ class NumMatrix {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        // Declare and initialize sum table
-        dp = new int[m + 1][n + 1];
+        // Declare and initialize area sum table
+        areas = new int[m + 1][n + 1];
 
         // Compute sum matrix
-        makeDP(m, n, dp);
+        computeAreas(m, n, areas);
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
 
-        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+        return areas[row2 + 1][col2 + 1] - areas[row1][col2 + 1] - areas[row2 + 1][col1] + areas[row1][col1];
     }
 }
 
